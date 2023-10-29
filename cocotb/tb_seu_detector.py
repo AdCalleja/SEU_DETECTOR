c3ae984fb3c3d67df7e6ceaa82f2242a20afc309
AdCalleja <Adrian.Calleja@ext.esa.int>
2023-10-29 22:05:42 +0000

    Basic test with all but FSM

--- cocotb/tb_seu_detector.py
+++ cocotb/tb_seu_detector.py
@@ -4,8 +4,8 @@ from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.clock import Clock
 
 @cocotb.test()
-async def tb_mmu(dut):
-    print("Executing SEU Detector")
+async def tb_count_bitflips(dut):
+    print("Executing count_bitflips")
     cocotb.start_soon(Clock(dut.clk, 10, units="ns").start()) # == CLK <= not CLK after 10 ns; -- 50 MHz 
     
 
@@ -15,14 +15,40 @@ async def tb_mmu(dut):
     # Write
     dut.rst_n.value = 1
     dut.w_mem_en.value = 1
-    await Timer(3000, units="ns")
+    # Force errors
+    await Timer(1500, units="ns")
+    dut.data.value = 0xFFFFFFFFFF
+    #await Timer(10, units="ns")
+    #dut.data.value = Release()
+    await Timer(1500, units="ns")
     # Reset = change of STATE in the state machine (Write -> Read)
     dut.rst_n.value = 0
     await Timer(10, units="ns")
     # Read and Check Errors
     dut.rst_n.value = 1
     dut.w_mem_en.value = 0
+    await Timer(1500, units="ns")
     await Timer(3000, units="ns")
 
+
+
+# @cocotb.test()
+# async def tb_seu_detector_no_fsm(dut):
+#     print("Executing SEU Detector NO FSM")
+#     cocotb.start_soon(Clock(dut.clk, 10, units="ns").start()) # == CLK <= not CLK after 10 ns; -- 50 MHz 
     
-    assert 1==1
+
+#     # Init
+#     dut.rst_n.value = 0
+#     await Timer(50, units="ns")
+#     # Write
+#     dut.rst_n.value = 1
+#     dut.w_mem_en.value = 1
+#     await Timer(3000, units="ns")
+#     # Reset = change of STATE in the state machine (Write -> Read)
+#     dut.rst_n.value = 0
+#     await Timer(10, units="ns")
+#     # Read and Check Errors
+#     dut.rst_n.value = 1
+#     dut.w_mem_en.value = 0
+#     await Timer(3000, units="ns")
