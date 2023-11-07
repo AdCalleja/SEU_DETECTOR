80c2fbaeecd92900838779d1df9de9d760d3487a
Adrian Calleja <adri.calleja.1999@gmail.com>
2023-11-07 20:53:22 +0100

    dtso correction to get interrups finally working :) Bad timing on c code config though

--- sw/src/seu_detector.c
+++ sw/src/seu_detector.c
@@ -9,17 +9,22 @@ int main()
 	uint32_t * addr = addr_init(uiofd);
 
 	printf("START\n");
-
-	generic_write(1, 2, addr); // n_reads
-	generic_write(2, 1, addr); // t_write = 1 seconds
-	generic_write(3, 1, addr); // t_write_resolution = 1 to have it in seconds
-	generic_write(0, 1, addr); // Start Experiment
-
-
 	uint32_t n_reads = generic_read(1, addr);
 	uint32_t t_write = generic_read(2, addr);
 	uint32_t t_write_resolution = generic_read(3, addr);
-	uint32_t on_off = generic_read(3, addr);
+	uint32_t on_off = generic_read(0, addr);
+	printf("CONFIG PRE:\n nreads = %d\n t_write = %d\n t_write_resolution = %d\n on_off = %d\n", n_reads, t_write, t_write_resolution, on_off);
+
+	generic_write(1, 2, addr); // n_reads
+	generic_write(2, 5, addr); // t_write = 1 seconds
+	generic_write(3, 0, addr); // t_write_resolution = 1 to have it in seconds
+	generic_write(0, 1, addr); // Start Experiment
+
+
+	n_reads = generic_read(1, addr);
+	t_write = generic_read(2, addr);
+	t_write_resolution = generic_read(3, addr);
+	on_off = generic_read(0, addr);
 	printf("CONFIG:\n nreads = %d\n t_write = %d\n t_write_resolution = %d\n on_off = %d\n", n_reads, t_write, t_write_resolution, on_off);
 
 
@@ -39,7 +44,7 @@ int main()
         if (nb >= (ssize_t)sizeof(info)) {
             /* Do something in response to the interrupt. */
             printf("Interrupt #%u!\n", info);
-			uint32_t bitflips = generic_read(5, addr);
+			uint32_t bitflips = generic_read(4, addr);
 			printf("Interrupt trigger, number of bitfips: %d\n", bitflips);
         }
 
