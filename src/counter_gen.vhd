a148fb8ea08b8ea1d398b57dfcba22688692aadd
AdCalleja <Adrian.Calleja@ext.esa.int>
2023-11-07 20:35:24 +0000

    Append to prev commit because I forgot to save -.-

--- src/counter_gen.vhd
+++ src/counter_gen.vhd
@@ -25,27 +25,27 @@ begin
   process (RESET_N, EN, CLK)
   begin
     if RESET_N = '1' then
-		if CLK = '1' and CLK'event then
-      if CLC = '0' then
-        if  EN = '1' then
-          if DIR = '1' then
-            if Counter >= CNT_TO then
-              Counter <= (others => '0');
+      if CLK = '1' and CLK'event then
+        if CLC = '0' then
+          if EN = '1' then
+            if DIR = '1' then
+              if Counter >= CNT_TO then
+                Counter <= (others => '0');
+              else
+                Counter <= Counter + 1;
+              end if;
             else
-              Counter <= Counter + 1;
-            end if;
-          else
-            if Counter >= CNT_TO then
-              Counter <= (others => '0');
-            else
-              Counter <= Counter - 1;
+              if Counter >= CNT_TO then
+                Counter <= (others => '0');
+              else
+                Counter <= Counter - 1;
+              end if;
             end if;
           end if;
+        else
+          Counter <= (others => '0');
         end if;
-      else
-        Counter <= (others => '0');
       end if;
-	end if;
     else
       Counter <= (others => '0');
     end if;
