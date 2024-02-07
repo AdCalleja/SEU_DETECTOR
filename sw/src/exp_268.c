b0b19eda41435fef963fbf8dfb4e19b6cf93edfd
AdCalleja <adri.calleja.1999@gmail.com>
2024-02-07 12:23:07 +0100

    Update exp version

--- sw/src/exp_268.c
+++ sw/src/exp_268.c
@@ -1,8 +1,8 @@
 #include "uio_generic.h"
 #include <stdint.h>
 #include <stdio.h>
-#include <time.h>
-#include <signal.h>
+//#include <time.h>
+//#include <signal.h>
 
 
 int main(int argc, char* argv[])
@@ -18,8 +18,9 @@ int main(int argc, char* argv[])
 	uint32_t T_WRITE = 1;
 	uint32_t T_WRITE_RESOLUTION = 1;
 	
+	// Get config from args
 	for(int i = 0; i < argc; i++){
-        printf("\nArgument %d = argv[%d] = %s\n", i , i, argv[i]);
+        //printf("\nArgument %d = argv[%d] = %s\n", i , i, argv[i]);
 		if (i == 1) {
 			N_READS = (uint32_t)strtoul(argv[i], NULL, 0);
 		} else if (i == 2)		{
@@ -40,8 +41,9 @@ int main(int argc, char* argv[])
 	uint32_t t_write = generic_read(2, addr);
 	uint32_t t_write_resolution = generic_read(3, addr);
 	uint32_t on_off = generic_read(0, addr);
-	printf("CONFIG PRE:\n nreads = %d\n t_write = %d\n t_write_resolution = %d\n on_off = %d\n", n_reads, t_write, t_write_resolution, on_off);
+	printf("preCONFIG:\n Number of reads per write: %d\n Time between writes: %d\n Resolution mode: %d\n Hardware Activation: %d\n", n_reads, t_write, t_write_resolution, on_off);
 
+	// CONFIGURE and START HW
 	generic_write(1, N_READS, addr); // n_reads
 	generic_write(2, T_WRITE, addr); // t_write = 1 seconds
 	generic_write(3, T_WRITE_RESOLUTION, addr); // t_write_resolution = 1 to have it in seconds
@@ -52,7 +54,9 @@ int main(int argc, char* argv[])
 	t_write = generic_read(2, addr);
 	t_write_resolution = generic_read(3, addr);
 	on_off = generic_read(0, addr);
-	printf("CONFIG:\n nreads = %d\n t_write = %d\n t_write_resolution = %d\n on_off = %d\n", n_reads, t_write, t_write_resolution, on_off);
+	printf("postCONFIG:\n Number of reads per write: %d\n Time between writes: %d\n Resolution mode: %d\n Hardware Activation: %d\n", n_reads, t_write, t_write_resolution, on_off);
+	const char *resolution = t_write_resolution == 1 ? "s" : "ms";
+	printf("Reading every %d %s. Writting every: %d %s.\n", (t_write/n_reads), resolution, t_write, resolution);
 
 
 
@@ -63,6 +67,8 @@ int main(int argc, char* argv[])
 	//strftime (buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", sTm);
 	//printf ("%s %s\n", buff, "Initial Time");
 
+	generic_exit(addr); // Close device
+
 	while(1){
 		//sleep(100);
 		uint32_t info = 1; /* unmask */
@@ -70,20 +76,24 @@ int main(int argc, char* argv[])
 
 		nb = read(uiofd, &info, sizeof(info));
         if (nb >= (ssize_t)sizeof(info)) {
+			//Open device
+			uint32_t * addr = addr_init(uiofd);
+
             /* Do something in response to the interrupt. */
             //printf("Interrupt #%u!\n", info);
 			uint32_t bitflips = generic_read(4, addr);
 			//printf("Interrupt trigger, number of bitfips: %d\n", bitflips);
 
+			generic_exit(addr); // Close device
+
 			//time_t now = time (0);
 			//sTm = gmtime (&now);
 			//strftime (buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", sTm);
-			printf ("%s %s %d\n", buff, "Events occurred:", bitflips);
+			//printf ("%s %s %d\n", buff, "Events occurred:", bitflips);
 			printf ("%s %d\n", "Events:", bitflips);
         }
 
 
 	}
 	printf("END\n");
-	generic_exit(addr);
 }
